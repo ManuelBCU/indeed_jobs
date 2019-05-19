6607ae824815fee02996c5fe6e7afe3e3f4c0dcc
ManuelBCU <43589178+ManuelBCU@users.noreply.github.com>
2019-05-19 13:17:47 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,5 +2,5 @@ import scraperwiki
 import lxml.html
 html = scraperwiki.scrape("https://www.indeed.co.uk/restaurant-jobs-in-England")
 root = lxml.html.fromstring(html)
-matchedlinks =root.cssselect("td div a title")
+matchedlinks =root.cssselect("td div a")
 print (matchedlinks)
