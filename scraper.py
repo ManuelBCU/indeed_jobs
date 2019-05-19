2daa63deff2f3bb05431e611d3ddb5e94951cf89
ManuelBCU <43589178+ManuelBCU@users.noreply.github.com>
2019-05-19 13:22:24 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,7 +2,7 @@ import scraperwiki
 import lxml.html
 html = scraperwiki.scrape("https://www.indeed.co.uk/restaurant-jobs-in-England")
 root = lxml.html.fromstring(html)
-matchedlinks =root.cssselect("table tbody td div a")
+matchedlinks =root.cssselect(" td div summary")
 for li in matchedlinks:
   #Store the text contents of li in a new variable listtext
   listtext = li.text_content()
