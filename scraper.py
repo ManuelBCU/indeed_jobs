8e02c1b0bc7b61b94cf0d4a2cb5033dd4c5116e9
ManuelBCU <43589178+ManuelBCU@users.noreply.github.com>
2019-05-19 13:01:24 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,11 +1,10 @@
-import request
+import requests
 import bs4
-import scraperwiki
 from bs4 import BeautifulSoup
 import pandas as pd
 import time
 
-URL = “https://www.indeed.co.uk/restaurant-jobs-in-England"
+URL = “https://www.indeed.co.uk/jobs?q=restaurant&l=England"
 #conducting a request of the stated URL above:
 page = requests.get(URL)
 #specifying a desired format of “page” using the html parser - this allows python to read the various components of the page, rather than treating it as one long string.
