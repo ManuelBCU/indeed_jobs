e5e48f3607589255f7b0d656f3449fac8134bf25
ManuelBCU <43589178+ManuelBCU@users.noreply.github.com>
2019-05-19 13:14:23 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,13 +1,5 @@
-import requests
-import bs4
-from bs4 import BeautifulSoup
-import pandas as pd
-import time
-
-URL = “https://www.indeed.co.uk/jobs?q=restaurant&l=England"
-#conducting a request of the stated URL above:
-page = requests.get(URL)
-#specifying a desired format of “page” using the html parser - this allows python to read the various components of the page, rather than treating it as one long string.
-soup = BeautifulSoup(page.text, “html.parser”)
-#printing soup in a more structured tree format that makes for easier reading
-print(soup.prettify())
+import scraperwiki
+import lxml.html
+html = scraperwiki.scrape("https://www.indeed.co.uk/restaurant-jobs-in-England")
+root = lxml.html.fromstring(html)
+root.cssselect("div a title")
